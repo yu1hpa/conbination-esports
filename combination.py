1e7c163177f2cdb5c29500834ca1dbf1495d847d
yu1hpa <yu.1hpa@gmail.com>
2021-10-13 18:17:27 +0900

    feat: add random

--- combination.py
+++ combination.py
@@ -1,13 +1,18 @@
 import itertools
 import os
+import random
 from contextlib import redirect_stdout
 
 print("空白区切りでメンバーの名前を入力")
 member = list(input().strip().split())
+out = []
 
 for mem in itertools.combinations_with_replacement(member, 2):
     if(mem[0] == mem[1]):
         with redirect_stdout(open(os.devnull, 'w')):
             print()
     else:
-        print(mem)
+        out.append(mem)
+
+for i in range(len(out)):
+    print(random.choice(out))
