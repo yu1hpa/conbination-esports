f2ac327574faf4478d249954368a17337fe80c72
yu1hpa <yu.1hpa@gmail.com>
2021-10-13 16:17:55 +0900

    feat: modify to not overlap user

--- combination.py
+++ combination.py
@@ -1,7 +1,13 @@
 import itertools
+import os
+from contextlib import redirect_stdout
 
-print("空白区切りでメンバーの名前を入力して")
+print("空白区切りでメンバーの名前を入力")
 member = list(input().strip().split())
 
-for mem in itertools.permutations(member, 2):
-    print(mem)
+for mem in itertools.combinations_with_replacement(member, 2):
+    if(mem[0] == mem[1]):
+        with redirect_stdout(open(os.devnull, 'w')):
+            print()
+    else:
+        print(mem)
